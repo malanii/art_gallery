de5cd277d14427cc6bbd633915063a82e5cc045c
Yana Malanii <miramis134@gmail.com>
2020-10-06 18:05:06 +0300

    small changes

--- js/scroll.js
+++ js/scroll.js
@@ -1,20 +1,13 @@
 $(document).ready(function () {
     let docWidth = $('body').width(),
         slidesWidth = $('#imgs').width(),
-        // rangeX = slidesWidth - docWidth,
         $images = $('#imgs');
-    // console.log(docWidth);
-
     $(document).on('mousemove', function (e) {
         let mouseX = e.pageX,
-            offset = mouseX / docWidth * slidesWidth - mouseX / 1.149;
-
+            offset = mouseX / docWidth * slidesWidth - mouseX / 1.12;
         $images.css({
             '-webkit-transform': 'translate3d(' + -offset + 'px,0,0)',
             'transform': 'translate3d(' + -offset + 'px,0,0)'
         });
-
-        // console.log(mouseX)
     });
-
 });
