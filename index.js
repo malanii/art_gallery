46ccc77dfc5d2440baba424eedfbb284673381bf
Yana Malanii <miramis134@gmail.com>
2020-10-07 14:37:57 +0300

    small changes

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ if ($('.img-item').length > 3) {
     $('.img-item:gt(2)').hide();
     $('.view-more-block').show();
 }
-
-$('.view-more-block').on('click', function () {
-
-    $('.img-item:gt(2)').toggle();
-
-    let docWidth = $('body').width(),
-        slidesWidth = $('#imgs').width(),
-        $images = $('#imgs');
+$(document).ready(function () {
     $(document).on('mousemove', function (e) {
         let mouseX = e.pageX,
-            offset = mouseX / docWidth * slidesWidth - mouseX / 0.9;
-        $images.css({
+            offset = mouseX / $('body').width() *  $('#imgs').width() - mouseX / 1.12;
+        $('#imgs').css({
             '-webkit-transform': 'translate3d(' + -offset + 'px,0,0)',
             'transform': 'translate3d(' + -offset + 'px,0,0)'
         });
-
     });
+});
+
+$('.view-more-block').on('click', function () {
+    $('.img-item:gt(2)').toggle();
     $('.view-more-block').hide();
 });
