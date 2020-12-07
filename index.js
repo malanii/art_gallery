753bf3db612baa5016bcc4f15a7ded8b5537e587
Yana Malanii <miramis134@gmail.com>
2020-12-07 12:27:57 +0200

    small changes / code style

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-if ($('.img-item').length > 3) {
-    $('.img-item:gt(2)').hide();
-    $('.view-more-block').show();
+if ($(".img-item").length > 3) {
+  $(".img-item:gt(2)").hide();
+  $(".view-more-block").show();
 }
 $(document).ready(function () {
-    $(document).on('mousemove', function (e) {
-        let mouseX = e.pageX,
-            offset = mouseX / $('body').width() *  $('#imgs').width() - mouseX / 1.12;
-        $('#imgs').css({
-            '-webkit-transform': 'translate3d(' + -offset + 'px,0,0)',
-            'transform': 'translate3d(' + -offset + 'px,0,0)'
-        });
+  $(document).on("mousemove", function (e) {
+    let mouseX = e.pageX,
+      offset =
+        (mouseX / $("body").width()) * $("#imgs").width() - mouseX / 1.12;
+    $("#imgs").css({
+      "-webkit-transform": "translate3d(" + -offset + "px,0,0)",
+      transform: "translate3d(" + -offset + "px,0,0)",
     });
+  });
 });
 
-$('.view-more-block').on('click', function () {
-    $('.img-item:gt(2)').toggle();
-    $('.view-more-block').hide();
+$(".view-more-block").on("click", function () {
+  $(".img-item:gt(2)").toggle();
+  $(".view-more-block").hide();
 });
